5efb297fc02ffa549a21aaffae4ca74c7faed66f
kewen <kewen@huawei.com>
2017-08-07 22:57:26 +0800

    This is the first Mocha Test Demo

--- UITestFramework/MochaTest.js
+++ UITestFramework/MochaTest.js
@@ -9,29 +9,11 @@ describe('Array', function() {
             assert.equal(-1, [1,2,3].indexOf(4));
         });
         it('should return 1 when the value is present', function() {
-            assert.equal(1, [1,2,3].indexOf(0));
-            console.log([1,2,3].indexOf(0));
+            assert.equal(1, [1,2,3].indexOf(2));
+        });
+        it('should return 0 when the value is present', function() {
+            assert.equal(0, [4,1,2,3].indexOf(4));
         });
     });
 });
 
-describe('User', function() {
-    describe('#save()', function() {
-        it('should save without error', function(done) {
-            var user = new User('Luna');
-            user.save(function(err) {
-                if (err) done(err);
-                else done();
-            });
-        });
-    });
-});
-
-describe('User', function() {
-    describe('#save()', function() {
-        it('should save without error', function(done) {
-            var user = new User('Luna');
-            user.save(done);
-        });
-    });
-});
