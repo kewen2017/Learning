3186cd8607c7f58f35be288e56936e4b88b07495
kewen <kewen@huawei.com>
2017-08-09 00:10:47 +0800

    add files

---
+++ UITestFramework/test/mochaTestDemo.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 34275 on 2017/7/21.
+ */
+var expect = require('chai').expect; //依赖于Node
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            expect([1,2,3].indexOf(4)).equal(-1)
+        });
+        it('should return 1 when the value is present', function() {
+            expect([1,2,3].indexOf(2)).equal(1)
+        });
+        it('should return 0 when the value is present', function() {
+            expect([1,2,3].indexOf(3)).not.equal(1)
+        });
+    });
+});
+
